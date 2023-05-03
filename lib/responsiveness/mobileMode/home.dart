1eda340649442635bc30d3ba71fe48bdd126ae3e
Gladys <gladys.wairimu290@gmail.com>
2023-05-03 08:28:44 +0300

    Create a mobile mode for the home page.

---
+++ lib/responsiveness/mobileMode/home.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
+
+  String makeupimg = 'makeup.png';
+  String skincareimg = 'skin-care.png';
+  String haircareimg = 'hair-care.png';
+  String oralcareimg = 'oral-care.png';
+  String perfumeimg = 'perfume.png';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      body: Stack(
+        children: [
+          Positioned(
+              top: 16,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                    'Shop',
+                  style: TextStyle(
+                    color: Colors.pinkAccent,
+                    fontSize: 40.0,
+                  ),
+                ),
+              ),
+            ),
+
+
+          Positioned(
+            top: 100,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: ListView(
+              children: [Table(
+                  columnWidths: {
+                    0: FlexColumnWidth(1), //The 1st column is twice as wide
+                    1: FlexColumnWidth(1), //as the second column
+                  },
+                  children: [
+                    //row1
+                    TableRow(
+                      children: [
+                          Padding(
+                            padding: const EdgeInsets.all(50.0),
+                              child: Container(
+                                height: 100,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                      child: Transform.scale(
+                                        scale: 3,
+                                        child: Image(image: AssetImage('resources/$makeupimg'),
+                                        ),
+                                      )
+                                  )
+                              ),
+
+                          ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                            child: Container(
+                              height: 50,
+                              width: 10,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.pinkAccent,
+                                  width: 3.0,
+                                ),
+                              ),
+                              child: Center(
+                                  child: Text(
+                                      'Makeup',
+                                    style: TextStyle(
+                                      fontSize: 22.0,
+                                      color: Colors.pinkAccent
+                                    ),
+                                  ),
+                              ),
+                            ),
+
+                        ),
+                      ],
+                    ),
+
+                    //row2
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                            child: Container(
+                              height: 50,
+                              width: 10,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.pinkAccent,
+                                  width: 3.0,
+                                ),
+                              ),
+                              child: Center(
+                                  child: Text(
+                                    'Skincare',
+                                    style: TextStyle(
+                                      fontSize: 22.0,
+                                      color: Colors.pinkAccent,
+                                    ),
+                                  ),
+
+                              ),
+                            ),
+
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                            child: Container(
+                                height: 100,
+                                child: Align(
+                                    alignment: Alignment.center,
+                                    child: Transform.scale(
+                                      scale: 3,
+                                      child: Image(image: AssetImage('resources/$skincareimg'),
+                                      ),
+                                    )
+                                )
+                            ),
+
+                        ),
+                      ],
+                    ),
+
+                    //row3
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Container(
+                              height: 100,
+                              child: Align(
+                                  alignment: Alignment.center,
+                                  child: Transform.scale(
+                                    scale: 3,
+                                    child: Image(image: AssetImage('resources/$haircareimg'),
+                                    ),
+                                  )
+                              )
+                          ),
+
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Container(
+                            height: 50,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.pinkAccent,
+                                width: 3.0,
+                              ),
+                            ),
+                            child: Center(
+                                child: Text(
+                                  'Haircare',
+                                  style: TextStyle(
+                                      fontSize: 22.0,
+                                      color: Colors.pinkAccent
+                                  ),
+                                ),
+                            ),
+                          ),
+
+                        ),
+                      ],
+                    ),
+
+                    //row4
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Container(
+                            height: 50,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.pinkAccent,
+                                width: 3.0,
+                              ),
+                            ),
+                            child: Center(
+                                child: Text(
+                                  'Oralcare',
+                                  style: TextStyle(
+                                    fontSize: 22.0,
+                                    color: Colors.pinkAccent,
+                                  ),
+                                ),
+                            ),
+                          ),
+
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Container(
+                              height: 100,
+                              child: Align(
+                                  alignment: Alignment.center,
+                                  child: Transform.scale(
+                                    scale: 3,
+                                    child: Image(image: AssetImage('resources/$oralcareimg'),
+                                    ),
+                                  )
+                              )
+                          ),
+
+                        ),
+                      ],
+                    ),
+
+                    //row5
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Container(
+                              height: 100,
+                              child: Align(
+                                  alignment: Alignment.center,
+                                  child: Transform.scale(
+                                    scale: 3,
+                                    child: Image(image: AssetImage('resources/$perfumeimg'),
+                                    ),
+                                  )
+                              )
+                          ),
+
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(50.0),
+                          child: Container(
+                            height: 50,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.pinkAccent,
+                                width: 3.0,
+                              ),
+                            ),
+                            child: Center(
+                                child: Text(
+                                  'Perfumes',
+                                  style: TextStyle(
+                                      fontSize: 22.0,
+                                      color: Colors.pinkAccent
+                                  ),
+                                ),
+                            ),
+                          ),
+
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ]
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+//< > ()  : {} _ $ + & ? %
