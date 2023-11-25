46019b879adc4ffb380717b5fd6fe8e41241e7b1
Gladys <gladys.wairimu290@gmail.com>
2023-11-25 19:41:53 +0300

    Create the page where users can shop for makeup products. Create a separate folder for a bottom navigation bar feature

---
+++ lib/pages/productsPage.dart
@@ -0,0 +1,13 @@
+class ProductsPage extends StatefulWidget {
+  const ProductsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductsPage> createState() => _ProductsPageState();
+}
+
+class _ProductsPageState extends State<ProductsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
