14de3d88e3758c17d5419ceff66360bc1b08898f
Gladys <gladys.wairimu290@gmail.com>
2023-11-27 19:00:26 +0300

    Commit cart page

---
+++ lib/pages/cartPage.dart
@@ -0,0 +1,13 @@
+//This page contains items a user intends to buy
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('You are in the CART page'),
+    );
+  }
+}
