e92b4ef656d2e732fd3287efccfdca5e806c8cc5
Gladys <gladys.wairimu290@gmail.com>
2023-11-28 02:55:19 +0300

    Create a template for the cart feature

---
+++ lib/models/cart.dart
@@ -0,0 +1,69 @@
+//This is class to create the template of a cart
+import 'package:glo/models/makeUp.dart';
+
+class Cart{
+  //list of makeup products for sale
+  List <MakeUp>makeupShop = [
+    MakeUp(
+      imagePath: 'lib/images/blush.png',
+      name: 'Item 1 ',
+      description: 'This is a description for item 1',
+      price: '1500'
+    ),
+    MakeUp(
+        imagePath: 'lib/images/brush.png',
+        name: 'Item 2 ',
+        description: 'This is a description for item 2',
+        price: '1100'
+    ),
+    MakeUp(
+        imagePath: 'lib/images/hair-shampoo.png',
+        name: 'Item 3 ',
+        description: 'This is a description for item 3',
+        price: '1700'
+    ),
+    MakeUp(
+        imagePath: 'lib/images/lipstick.png',
+        name: 'Item 4 ',
+        description: 'This is a description for item 4',
+        price: '1500'
+    ),
+    MakeUp(
+        imagePath: 'lib/images/lotion.png',
+        name: 'Item 5 ',
+        description: 'This is a description for item 5',
+        price: '500'
+    ),
+    MakeUp(
+        imagePath: 'lib/images/perfume.png',
+        name: 'Item 6 ',
+        description: 'This is a description for item 6',
+        price: '300'
+    ),
+  ];
+
+  //list of items in the user's cart
+  List<MakeUp> userCart = [];
+
+  //getter method for list of shoes for sale
+  List<MakeUp> getShoeList(){
+    return makeupShop;
+  }
+
+  //getter method for cart items
+  List<MakeUp> getUserCart(){
+    return userCart;
+  }
+
+  //method to add items to the cart
+  void addItemToCart(MakeUp makeUp){
+    userCart.add(makeUp);
+  }
+
+  //method to remove items from the cart
+  void removeItemFromCart(MakeUp makeUp){
+    userCart.remove(makeUp);
+  }
+}
+
+//< > ()  : {} _ $ + & ? % !
