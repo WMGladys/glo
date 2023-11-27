bfeaee3af9bbe42997346209e0b2e5044afac63e
Gladys <gladys.wairimu290@gmail.com>
2023-11-27 19:23:29 +0300

    Create a template for the makeup object.

---
+++ lib/models/makeup.dart
@@ -0,0 +1,17 @@
+//This is a class to create a product and its attributes like image, price etc.
+
+class MakeUp{
+  final String name;
+  final String price;
+  final String imagePath;
+  final String description;
+
+  MakeUp({
+    required this.name,
+    required this.price,
+    required this.imagePath,
+    required this.description
+  });
+}
+
+//< > ()  : {} _ $ + & ? % !
