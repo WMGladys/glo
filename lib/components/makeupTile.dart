7366c3f91796326916c4719aa9e02d0ad1237e21
Gladys <gladys.wairimu290@gmail.com>
2023-11-28 00:27:22 +0300

    Modify the position of items in the tile

--- lib/components/makeupTile.dart
+++ lib/components/makeupTile.dart
@@ -12,40 +12,88 @@ class MakeupTile extends StatelessWidget {
       margin: EdgeInsets.only(left: 25),
       width: 280,
       decoration: BoxDecoration(
-        color: Colors.grey[400],
+        color: Colors.grey[300],
         borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          //picture
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(makeUp.imagePath)
-          ),
-          //description
-          Text(
-              makeUp.description,
-              style: TextStyle(color: Colors.black),
-          ),
-          //price
+          //icons to like and add items
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              //column with name and price
+              //like icon
               Column(
                 children: [
-                  Text(makeUp.name),
-                  Text(makeUp.price),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Icon(Icons.favorite_border),
+                  ),
                 ],
               ),
-              //create plus button to add to cart
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Icon(Icons.add_shopping_cart),
+              //add to cart icon
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Icon(Icons.add_shopping_cart),
+                  ),
+                ],
               ),
             ],
           ),
+
+          //picture
+          Container(
+            child: Transform.translate(
+              offset: Offset(0.0, -100.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(makeUp.imagePath)
+              ),
+            ),
+          ),
+          //description
+          Container(
+            child: Transform.translate(
+              offset: Offset(0.0, -80),
+              child: Text(
+                //makeUp.description,
+                makeUp.name,
+                style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+
+          ),
+          //price
+          //Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //children: [
+              //column with name and price
+          Container(
+            child: Transform.translate(
+              offset: Offset(0.0, -70),
+              child: Column(
+                children: [
+                  //Text(makeUp.name),
+                  Text(makeUp.description, style: TextStyle(fontSize: 18),),
+                  Text(makeUp.price, style: TextStyle(fontSize: 18),),
+                ],
+              ),
+            ),
+
+          ),
+              //create plus button to add to cart
+              // Column(
+              //   children: [
+              //     Container(
+              //       padding: EdgeInsets.all(20),
+              //       child: Icon(Icons.add_shopping_cart),
+              //     ),
+              //   ],
+              // ),
+            //],
+          //),
 
           //button to add to cart
         ],
