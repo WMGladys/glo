f467284fa7cce21781c371c09750bb1901c5f963
Gladys Wairimu <72851023+WMGladys@users.noreply.github.com>
2024-01-07 13:33:43 +0300

    Update authentication.dart

--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MyApp());
-// }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
